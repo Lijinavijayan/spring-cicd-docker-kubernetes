e687304f6fb166914d59a8f6f084e49e08bf4caa
unknown <lijinvtechie@gmail.com>
2022-08-14 21:09:10 +0530

    First commit for my cicd pipeline

---
+++ src/main/java/com/srm/devops/SpringCicdDockerKubernetesApplication.java
@@ -0,0 +1,13 @@
+package com.srm.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCicdDockerKubernetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCicdDockerKubernetesApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/srm/devops/controller/CicdPipeline.java
@@ -0,0 +1,27 @@
+package com.srm.devops.controller;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cicdPipeline")
+public class CicdPipeline {
+
+	@GetMapping("/webMethod")
+	public String webMethod()
+	{
+		return "Hello from Web application!!!";
+	}
+	
+	
+}
